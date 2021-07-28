4cc1ef2440086f5806e6f7d14d612bbfd136ed76
jenjensoft <86238696+jenjensoft@users.noreply.github.com>
2021-07-28 19:03:00 +0900

    Update Ajaxtest1.js

--- Ajaxtest1.js
+++ Ajaxtest1.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 $.ajax({url: "https://jenjensoft.github.io/jenjensoft3/save.txt" , success: function(result) {
 
 
-$("#div1").html(");
+$("#div1").html("kkkkkkkk");
 
 }})});
 
